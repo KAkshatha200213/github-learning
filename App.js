264a5d82a660e1d615b537b047ca5490a8cbb6d6
K Akshatha <kakshathakakshatha21@gmail.com>
2026-09-07 18:23:23 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import {useState} from "react";
+function App(){
+    const [name, setName] = useState("Kanha");
+    return (
+        <>
+        <input
+         type="text"
+          value={name} 
+          onChange={(e) => setName(e.target.value)}
+           />
+        <button onClick={() => alert(name)}>Greet</button>
+        </>
+    );
+}
+
+export default App;
